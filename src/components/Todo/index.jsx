56f4ad5e1e23c4bcd69fdc805110be9253a8fe0d
ZACH HE (ITA-ISDC-ISD-OOCLL/ZHA) <HEZA2@OOCL.COM>
2020-08-05 15:12:41 +0800

    [zach]feature:add divider in todo list by antd

--- src/components/Todo/index.jsx
+++ src/components/Todo/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './index.css'
 import { deleteTodo,modifyTodo } from '../../store/api'
-
+import { Divider } from 'antd';
 
 class ToDo extends React.Component {
 
@@ -28,6 +28,7 @@ class ToDo extends React.Component {
                 <p onClick={this.complete} className={this.props.todo.status ? "completed" : "uncompleted"}>
                     {this.props.todo.text}---------<button onClick={this.deleteTodo}>X</button>
                 </p>
+                <Divider />
             </div>
         )
     }
--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import Todo from '../Todo'
-
+import { Divider } from 'antd';
 class ToDoList extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
     }
 
     render() {
         return (<React.Fragment>
+            <Divider />
             <h2>todo list</h2>
+            <Divider />
             <div>
                 {this.props.todoList.map((todo, index) =>
-                    <Todo key={index} todo={todo} deleteTodo={this.props.deleteTodo} changeStatus={this.props.changeStatus}/>
+                    <Todo key={index} todo={todo} deleteTodo={this.props.deleteTodo} changeStatus={this.props.changeStatus} />
                 )}
             </div>
         </React.Fragment>)
