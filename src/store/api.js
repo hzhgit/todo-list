710fef6704c3361d82fb216bfb02c1cc7495820d
zach-he <971499086@qq.com>
2020-08-06 23:12:22 +0800

    [zach]feature:use my backend api

--- src/store/api.js
+++ src/store/api.js
@@ -1,31 +1,32 @@
 import axios from 'axios'
 
+const baseUrl = 'http://localhost:8080/todos'
 export function getTodoList() {
   return axios({
     method: 'get',
-    url: 'https://5e9ec500fb467500166c4658.mockapi.io/todos'
+    url: baseUrl
   })
 }
 
 export function deleteTodo(id) {
   return axios({
     method: 'delete',
-    url: `https://5e9ec500fb467500166c4658.mockapi.io/todos/${id}`
+    url: `${baseUrl}/${id}`
   })
 }
 
-export function addTodo(text,status) {
+export function addTodo(text, status) {
   return axios({
     method: 'post',
-    url: `https://5e9ec500fb467500166c4658.mockapi.io/todos`,
-    data: {content:text,status:status}
+    url: baseUrl,
+    data: { content: text, status: status }
   })
 }
 
 export function modifyTodo(id, todo) {
   return axios({
     method: 'put',
-    url: `https://5e9ec500fb467500166c4658.mockapi.io/todos/${id}`,
+    url: `${baseUrl}/${id}`,
     data: todo
   })
 }
