7ae9f5658b3f83317319cd690b574bfefb5a3bb9
hzhgit <971499086@qq.com>
2020-08-07 08:27:57 +0800

    [zach]feature:add menu by ant design

--- src/App.js
+++ src/App.js
@@ -5,17 +5,23 @@ import TodoFormContainer from "./containers/TodoFormContainer";
 import DoneListContainer from "./containers/DoneListContainer"
 import { HashRouter, Link, Route } from 'react-router-dom';
 import 'antd/dist/antd.css';
+import { Menu } from 'antd';
 
 
 function App() {
   return (
     <div className="App">
       <HashRouter>
-        <span><Link to='/'>ALL TODO LIST</Link></span>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <span><Link to='/done-todo-list'>DONE TODO LIST</Link></span>
-        
-        <TodoFormContainer></TodoFormContainer>
+        <Menu mode="horizontal">
+          <Menu.Item key="all">
+            <Link to='/'>ALL TODO LIST</Link>
+          </Menu.Item>
+          <Menu.Item key="app">
+            <Link to='/done-todo-list'>DONE TODO LIST</Link>
+          </Menu.Item>
+        </Menu>
+
+        <TodoFormContainer/>
 
         <Route path="/" exact component={TodoListContainer} />
         <Route path="/done-todo-list" component={DoneListContainer} />
